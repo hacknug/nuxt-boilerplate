74dbc6919c727adffbde28f202b7a60d89c09b76
Nestor Vera <nestorvera@me.com>
2021-06-02 13:07:32 +0200

    Add base Tailwind config utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+// const _ = require('lodash')
+// const plugin = require('tailwindcss/plugin')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const pxToRem = (px, base = 16) => `${px / base}rem`
+const buildScaleFromArray = (I, cb) => I.reduce((a, i) => ({ ...a, [i]: cb ? cb(i) : i }), {})
+const buildScaleFromSteps = (step = 4, limit = 64, _cb = pxToRem) => {
+  const scale = { 0: '0px' }
+  Array(limit / step).fill()
+    .map((value, key) => _cb((key + 1) * step))
+    .forEach((value, key) => { scale[(key + 1) * step] = value })
+  return scale
+}
+
 module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+      },
+    },
   },
 
   variants: {
