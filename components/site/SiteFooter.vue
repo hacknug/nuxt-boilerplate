b79b2f3cc092fd1636a4bdbdc97bb989c12ebfa6
Nestor Vera <nestorvera@me.com>
2021-06-03 19:10:34 +0200

    Cleanup default components

---
+++ components/site/SiteFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="w-full">
+    <div class="container">
+      <p>{{ $config.siteName }}</p>
+      <p>{{ $config.siteDescription }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter',
+}
+</script>
---
+++ components/site/SiteHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="w-full">
+    <div :class="['container', 'flex items-center justify-between']">
+      <div>
+        <NuxtLink to="/">{{ $config.siteName }}</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/">{{ $config.siteDescription }}</NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader',
+}
+</script>
