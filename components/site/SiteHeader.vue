887944b20642476a795aa43dc65d61ebda1a4131
Nestor Vera <nestorvera@me.com>
2021-06-04 17:55:29 +0200

    Fix `siteName` link not using `localePath` to respect the user's locale

--- components/site/SiteHeader.vue
+++ components/site/SiteHeader.vue
@@ -2,7 +2,7 @@
   <header class="w-full">
     <div :class="['container', 'flex items-center justify-between']">
       <div class="space-x-2">
-        <NuxtLink class="text-[blue] dark:text-[lime]" to="/">{{ $config.siteName }}</NuxtLink>
+        <NuxtLink class="text-[blue] dark:text-[lime]" :to="localePath('/')">{{ $config.siteName }}</NuxtLink>
       </div>
       <div class="space-x-2">
         <template v-for="item in $i18n.locales">
